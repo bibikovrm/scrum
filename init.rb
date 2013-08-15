de7883db28a9f3d2ebccd8034041c502ea09a7bb
Emilio González Montaña <emilio@ociotec.com>
2013-08-15 11:02:39 +0000

    #702. * Empty Redmine plugin with @init.rb@ file.
    git-svn-id: https://svn.ociotec.com/Software/Web/Redmine/plugins/scrum/trunk@1563 1633e602-76eb-4acd-8a1c-850ebfcfe024

---
+++ init.rb
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+
+Redmine::Plugin.register :scrum do
+  name        "Scrum Redmine plugin"
+  author      "Emilio González Montaña"
+  description "This plugin for Redmine allows to follow Scrum methodology with Redmine projects"
+  version     "0.1.0"
+  url         "https://redmine.ociotec.com/projects/redmine-plugin-scrum"
+  author_url  "http://ociotec.com"
+end
