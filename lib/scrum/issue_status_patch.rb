99986f5c0e52c3f2c809b6aabbb7be6267340718
Emilio González Montaña <emilio@ociotec.com>
2013-10-22 20:27:14 +0000

    #767 * Modify IssueStatus model patch to use position.
    git-svn-id: https://svn.ociotec.com/Software/Web/Redmine/plugins/scrum/trunk@1610 1633e602-76eb-4acd-8a1c-850ebfcfe024

--- lib/scrum/issue_status_patch.rb
+++ lib/scrum/issue_status_patch.rb
@@ -6,7 +6,8 @@ module Scrum
       base.class_eval do
 
         def self.task_statuses
-          IssueStatus.find(Setting.plugin_scrum[:task_statuses].collect{|status| status.to_i})
+          ids = Setting.plugin_scrum[:task_statuses].collect{|status| status.to_i} || []
+          IssueStatus.find(ids, :order => "position ASC")
         end
 
       end
