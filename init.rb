b1bb257c8eaed782483b61f58ba3ed0f85b06856
Emilio González Montaña <emilio@ociotec.com>
2014-11-25 22:15:03 +0000

    #911 * Added license file as txt. * Added license header as a comment to Ruby source code. * Added link to plugin footer for license. * Added info about license in plugin settings page.
    git-svn-id: https://svn.ociotec.com/Software/Web/Redmine/plugins/scrum/trunk@1721 1633e602-76eb-4acd-8a1c-850ebfcfe024

--- init.rb
+++ init.rb
@@ -1,5 +1,12 @@
 # encoding: UTF-8
 
+# Copyright © Emilio González Montaña
+# Licence: Attribution & no derivates
+#   * Attribution to the plugin web page URL should be done if you want to use it.
+#     https://redmine.ociotec.com/projects/redmine-plugin-scrum
+#   * No derivates of this plugin (or partial) are allowed.
+# Take a look to licence.txt file at plugin root folder for further details.
+
 # This plugin should be reloaded in development mode.
 if (Rails.env == "development")
   ActiveSupport::Dependencies.autoload_once_paths.reject!{|x| x =~ /^#{Regexp.escape(File.dirname(__FILE__))}/}
