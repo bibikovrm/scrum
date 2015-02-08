e5d02e1770ac19205632615a78c9a68ac14eb532
Emilio González Montaña <emilio@ociotec.com>
2015-02-08 21:56:58 +0000

    #967 * Added WIKI syntax to render links to Sprint stats, also alias for current/latest Sprint (instead of Sprint ID).
    git-svn-id: https://svn.ociotec.com/Software/Web/Redmine/plugins/scrum/trunk@1758 1633e602-76eb-4acd-8a1c-850ebfcfe024

--- lib/scrum/application_helper_patch.rb
+++ lib/scrum/application_helper_patch.rb
@@ -23,6 +23,17 @@ module Scrum
           return link_to(label, path)
         end
 
+        def link_to_sprint_stats(sprint, include_project_prefix = false)
+          if sprint.is_product_backlog
+            return nil
+          else
+            path = stats_sprint_path(sprint)
+            label = l(:label_sprint_stats_name, :name => sprint.name)
+            label = "#{sprint.project.name}:#{label}" if include_project_prefix
+            return link_to(label, path)
+          end
+        end
+
         def link_to_sprint_burndown(sprint, include_project_prefix = false)
           if sprint.is_product_backlog
             label = l(:label_product_backlog_burndown_chart)
@@ -43,7 +54,7 @@ module Scrum
 
         def parse_redmine_links_with_scrum(text, default_project, obj, attr, only_path, options)
           result = parse_redmine_links_without_scrum(text, default_project, obj, attr, only_path, options)
-          text.gsub!(%r{([\s\(,\-\[\>]|^)(!)?(([a-z0-9\-_]+):)?(sprint|product-backlog|sprint-burndown|product-backlog-burndown|release-plan)?((#)(\d*))(?=(?=[[:punct:]][^A-Za-z0-9_/])|,|\s|\]|<|$)}) do |m|
+          text.gsub!(%r{([\s\(,\-\[\>]|^)(!)?(([a-z0-9\-_]+):)?(sprint|sprint-burndown|sprint-stats|product-backlog|product-backlog-burndown|release-plan)?((#)((\d*)|(current|latest)))(?=(?=[[:punct:]][^A-Za-z0-9_/])|,|\s|\]|<|$)}) do |m|
             leading, project_identifier, element_type, separator, element_id_text = $1, $4, $5, $7, $8
             link = nil
             project = default_project
@@ -52,21 +63,30 @@ module Scrum
             end
             if project and element_type and element_id_text
               element_id = element_id_text.to_i
+              include_project_prefix = (project != default_project)
+              case element_type
+                when "sprint", "sprint-burndown", "sprint-stats"
+                  if ((element_id_text == "latest") or (element_id_text == "current"))
+                    sprint = project.last_sprint
+                  else
+                    sprint = project.sprints.find_by_id(element_id)
+                  end
+                when "product-backlog", "product-backlog-burndown"
+                  sprint = project.product_backlog
+              end
               case element_type
                 when "sprint"
-                  sprint = project.sprints.find_by_id(element_id)
-                  link = link_to_sprint(sprint, project != default_project) unless sprint.nil?
-                when "product-backlog"
-                  sprint = project.product_backlog
-                  link = link_to_sprint(sprint, project != default_project) unless sprint.nil?
+                  link = link_to_sprint(sprint, include_project_prefix) unless sprint.nil?
                 when "sprint-burndown"
-                  sprint = project.sprints.find_by_id(element_id)
-                  link = link_to_sprint_burndown(sprint, project != default_project) unless sprint.nil?
+                  link = link_to_sprint_burndown(sprint, include_project_prefix) unless sprint.nil?
+                when "sprint-stats"
+                  link = link_to_sprint_stats(sprint, include_project_prefix) unless sprint.nil?
+                when "product-backlog"
+                  link = link_to_sprint(sprint, include_project_prefix) unless sprint.nil?
                 when "product-backlog-burndown"
-                  sprint = project.product_backlog
-                  link = link_to_sprint_burndown(sprint, project != default_project) unless sprint.nil?
+                  link = link_to_sprint_burndown(sprint, include_project_prefix) unless sprint.nil?
                 when "release-plan"
-                  link = link_to_release_plan(project, project != default_project) unless project.product_backlog.nil?
+                  link = link_to_release_plan(project, include_project_prefix) unless project.product_backlog.nil?
               end
             end
             (leading + (link || "#{project_identifier}#{element_type}#{separator}#{element_id_text}"))
