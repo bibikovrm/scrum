f87307c8f2b52dea82bad6a893f61c70d9e96828
Emilio González Montaña <emilio@ociotec.com>
2016-12-10 10:21:54 +0000

    #1275 * Increase plugin version to v0.17.0. * Separate project settings into 2 tabs: sprints & product backlogs.
    git-svn-id: https://svn.ociotec.com/Software/Web/Redmine/plugins/scrum/trunk@1995 1633e602-76eb-4acd-8a1c-850ebfcfe024

--- lib/scrum/projects_helper_patch.rb
+++ lib/scrum/projects_helper_patch.rb
@@ -15,13 +15,17 @@ module Scrum
         def project_settings_tabs_with_scrum
           tabs = project_settings_tabs_without_scrum
           if User.current.allowed_to?(:manage_sprints, @project)
-            index = tabs.index({:name => "versions", :action => :manage_versions,
-                                :partial => "projects/settings/versions",
+            index = tabs.index({:name => 'versions', :action => :manage_versions,
+                                :partial => 'projects/settings/versions',
                                 :label => :label_version_plural})
             if index
               tabs.insert(index,
-                          {:name => "sprints", :action => :edit_sprints,
-                           :partial => "projects/settings/sprints",
+                          {:name => 'product_backlogs', :action => :edit_sprints,
+                           :partial => 'projects/settings/product_backlogs',
+                           :label => :label_product_backlog_plural})
+              tabs.insert(index,
+                          {:name => 'sprints', :action => :edit_sprints,
+                           :partial => 'projects/settings/sprints',
                            :label => :label_sprint_plural})
               tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}
             end
