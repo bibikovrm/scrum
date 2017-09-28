5a674c94f077aec056ddba2dcc91bc4e355b68de
Emilio González Montaña <emilio@ociotec.com>
2017-09-28 23:12:46 +0200

    [#1457] Fix custom fields in Sprint board

--- lib/scrum/tracker_patch.rb
+++ lib/scrum/tracker_patch.rb
@@ -47,7 +47,7 @@ module Scrum
         end
 
         def custom_field?(custom_field)
-          Scrum::Setting.tracker_field?(self.id, custom_field, Scrum::Setting::TrackerFields::CUSTOM_FIELDS) or custom_field.is_required
+          Scrum::Setting.tracker_field?(self.id, custom_field.id, Scrum::Setting::TrackerFields::CUSTOM_FIELDS) or custom_field.is_required
         end
 
       end
